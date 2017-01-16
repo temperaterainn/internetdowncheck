1fe1a4c0e03c150440c7cfa90f1f387ff0ad018d
Arian Nasr <arian@ariannsr.xyz>
2017-01-16 18:55:23 -0500

    Added Code
    Added code to make the script functional.

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
+import os
+import smtplib
 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("Your Email", "Password")
+hostname = "https://google.com"
+msg = "The internet is down."
+
+response = os.system("ping -c 1 " + hostname)
+
+if response == 0:
+  pass
+else:
+  server.sendmail("Your Email", "Email to send message", msg)
+  server.quit()
